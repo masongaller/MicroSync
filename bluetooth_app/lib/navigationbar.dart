25b6a056f116a3c8f45672ef4ba62d90dff2fc04
masongaller <a.galler@wustl.edu>
2024-01-29 21:27:44 -0600

    Added a welcome screen

---
+++ bluetooth_app/lib/navigationbar.dart
@@ -0,0 +1,91 @@
+import 'package:bluetooth_app/home.dart';
+import 'package:bluetooth_app/saved.dart';
+import 'package:bluetooth_app/settings.dart';
+import 'package:flutter/material.dart';
+
+class MyNavigationBar extends StatefulWidget {
+  const MyNavigationBar({super.key});
+
+  @override
+  State<MyNavigationBar> createState() => _MyNavigationBarState();
+}
+
+class _MyNavigationBarState extends State<MyNavigationBar> {
+  int currentPageIndex = 0;
+  String appBarTitle = "Hi";
+  final PageController _pageController = PageController(initialPage: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: theme.colorScheme.inversePrimary,
+        title: Text(appBarTitle),
+      ),
+      bottomNavigationBar: NavigationBar(
+        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+        onDestinationSelected: _onDestinationSelected,
+        indicatorColor: theme.colorScheme.inversePrimary,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.bookmark),
+            icon: Icon(Icons.bookmark_outline),
+            label: 'Saved',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.settings),
+            icon: Icon(Icons.settings_outlined),
+            label: 'Settings',
+          ),
+        ],
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: _onPageChanged,
+        children: const [
+          MyHomePage(),
+          MySavedPage(),
+          MySettingsPage(),
+        ],
+      ),
+    );
+  }
+
+  void _onDestinationSelected(int index) {
+    setState(() {
+      currentPageIndex = index;
+      _pageController.jumpToPage(index);
+      appBarTitle = _getAppBarTitle(index);
+    });
+  }
+
+  void _onPageChanged(int index) {
+    setState(() {
+      currentPageIndex = index;
+    });
+  }
+
+  String _getAppBarTitle(int index) {
+    switch (index) {
+      case 0:
+        return 'Home';
+      case 1:
+        return 'Saved';
+      case 2:
+        return 'Settings';
+      default:
+        return '';
+    }
+  }
+}
+
+
+
---
+++ bluetooth_app/lib/welcome.dart
@@ -0,0 +1,118 @@
+import 'package:bluetooth_app/navigationbar.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  double _opacity = 1;
+  double _padding = 1;
+  double _iconOpacity = 0;
+
+  static const textCurve = Curves.bounceOut;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 0), () {
+      setState(() {
+        _padding = 0.3;
+      });
+    });
+
+    Future.delayed(const Duration(seconds: 1), () {
+      setState(() {
+        _iconOpacity = 1;
+      });
+    });
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        PageRouteBuilder(
+          pageBuilder: (context, animation, secondaryAnimation) =>
+              MyNavigationBar(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            const begin = Offset(0.0, 1.0);
+            const end = Offset.zero;
+            const curve = Curves.easeInOutQuart;
+
+            var tween =
+                Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+            var offsetAnimation = animation.drive(tween);
+
+            return SlideTransition(position: offsetAnimation, child: child);
+          },
+          transitionDuration: const Duration(seconds: 1),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    final double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: AnimatedContainer(
+        duration: const Duration(seconds: 1),
+        color: theme.colorScheme.inversePrimary,
+        child: AnimatedOpacity(
+          duration: const Duration(seconds: 1),
+          opacity: _opacity,
+          child: Center(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                TweenAnimationBuilder<double>(
+                  tween: Tween<double>(begin: 1.0, end: _padding),
+                  curve: textCurve,
+                  duration: const Duration(milliseconds: 1000),
+                  builder: (BuildContext context, double value, Widget? child) {
+                    return Padding(
+                      padding: EdgeInsets.only(bottom: screenHeight * value),
+                      child: child,
+                    );
+                  },
+                  child: Text(
+                    'Welcome',
+                    style: GoogleFonts.oswald(
+                      textStyle: TextStyle(
+                        fontSize: 75,
+                        color: theme.colorScheme.onPrimary,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                TweenAnimationBuilder<double>(
+                  curve: textCurve,
+                  tween: Tween<double>(begin: 0.0, end: _iconOpacity),
+                  duration: const Duration(seconds: 1),
+                  builder: (BuildContext context, double value, Widget? child) {
+                    return Opacity(
+                      opacity: value,
+                      child: child,
+                    );
+                  },
+                  child: Icon(
+                    Icons.bluetooth,
+                    size: 100,
+                    color: theme.colorScheme.onPrimary,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

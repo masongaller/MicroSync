d7e96a3fd0c2eae916e03dc888ca602786af8e72
masongaller <a.galler@wustl.edu>
2024-02-07 20:23:26 -0600

    Added a delay to the stream to simulate real data

--- bluetooth_app/lib/shareddata.dart
+++ bluetooth_app/lib/shareddata.dart
@@ -7,8 +7,16 @@ class SharedData extends ChangeNotifier {
 
   List<Point> get points => _points;
 
+  // simulate a data source
+  void simulateDataStream() async {
+    for (var i = 0; i < 100; i++) {
+      await Future.delayed(const Duration(seconds: 2));
+      _points.add(Point(_points.length, Random().nextDouble() * 10));
+      notifyListeners(); // This will alert the widgets that are listening to this model.
+    }
+  }
+
   void addPoint() {
-    _points.add(Point(_points.length, Random().nextDouble() * 10));
-    notifyListeners();  // This will alert the widgets that are listening to this model.
+    simulateDataStream();
   }
 }

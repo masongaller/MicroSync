6b58b1bd454e09018ae9046a566e803dbbf0c92d
masongaller <a.galler@wustl.edu>
2024-03-19 23:22:55 -0500

    Can collapse columns in the table

--- bluetooth_app/lib/table.dart
+++ bluetooth_app/lib/table.dart
@@ -9,43 +9,76 @@ class MyTablePage extends StatefulWidget {
   State<MyTablePage> createState() => _MyTablePageState();
 }
 
-class _MyTablePageState extends State<MyTablePage>
-    with AutomaticKeepAliveClientMixin<MyTablePage> {
+class _MyTablePageState extends State<MyTablePage> with AutomaticKeepAliveClientMixin<MyTablePage> {
+  bool initOnce = true;
+  List<bool> visibleColumns = [];
+
   @override
   Widget build(BuildContext context) {
     super.build(context); // Invoke the overridden method
 
-    final watchPoints = context.watch<
-        SharedBluetoothData>(); //Use context.watch<T>() when the widget needs to rebuild when the model changes.
-    final readPoints = context.read<
-        SharedBluetoothData>(); //To modify the data without rebuilding the widget
+    final watchPoints = context
+        .watch<SharedBluetoothData>(); //Use context.watch<T>() when the widget needs to rebuild when the model changes.
+    final readPoints = context.read<SharedBluetoothData>(); //To modify the data without rebuilding the widget
 
     if (watchPoints.fullHeaders.isNotEmpty) {
+      if (initOnce) {
+        initOnce = false;
+        visibleColumns = List.generate(watchPoints.headers.length, (index) => true);
+      }
+
       return Scaffold(
         body: Center(
           child: DataTable2(
             columnSpacing: 12,
             horizontalMargin: 12,
-            minWidth: 600,
+            minWidth: 1000,
             fixedTopRows: 1,
+            isHorizontalScrollBarVisible: true,
+            isVerticalScrollBarVisible: true,
             columns: List<DataColumn>.generate(
               watchPoints.headers.length,
-              (int index) => DataColumn(
+              (int index) => DataColumn2(
+                fixedWidth: visibleColumns[index] ? 175 : 55,
                 label: Expanded(
-                  child: Text(
-                    watchPoints.headers[index],
-                    style: const TextStyle(fontStyle: FontStyle.italic),
+                  child: Row(
+                    children: [
+                      Icon(
+                        !visibleColumns[index] ? Icons.arrow_drop_up : Icons.arrow_drop_down,
+                      ),
+                      Visibility(
+                        visible: visibleColumns[index],
+                        child: Flexible(
+                          child: Text(
+                            watchPoints.headers[index],
+                            style: const TextStyle(fontStyle: FontStyle.italic),
+                            overflow: TextOverflow.ellipsis, // Apply ellipsis for long text
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
+                onSort: (columnIndex, ascending) {
+                  setState(() {
+                    visibleColumns[index] = !visibleColumns[index];
+                  });
+                },
               ),
             ),
             rows: watchPoints.rows
                 .where((rowData) => !(rowData.length == 1)) //Do not include reboot rows
                 .map((rowData) {
               return DataRow(
-                cells: rowData.skip(3).map((cellData) {
-                  return DataCell(Text(cellData.toString()));
-                }).toList(),
+                cells: List<DataCell>.generate(rowData.length - 3, (cellIndex) {
+                  bool cellVisibility = visibleColumns[cellIndex];
+                  return DataCell(
+                    Visibility(
+                      visible: cellVisibility,
+                      child: Text(rowData[cellIndex + 3].toString()),
+                    ),
+                  );
+                }),
               );
             }).toList(),
           ),

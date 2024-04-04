a6a636bd0eb86bcf525e65dd4cb87ff0cb803fb6
masongaller <a.galler@wustl.edu>
2024-04-03 21:12:48 -0500

    Renames and deletes

--- MicroSync/macos/Flutter/GeneratedPluginRegistrant.swift
+++ MicroSync/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,13 @@ import Foundation
 import dynamic_color
 import flutter_blue_plus
 import path_provider_foundation
+import share_plus
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
